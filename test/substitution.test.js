6bbe8e10ea50dc6203d724ee0f094d3b2fe3958e
Andy Hotzler <ahotzler@gmail.com>
2021-01-04 11:43:26 -0600

    Wrote basic tests for substitution cipher; one not passing

--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,34 @@
 // Write your tests here!
+
+const expect = require('chai').expect;
+const substitution = require('../src/substitution');
+
+describe('substitution', () => {
+    it ('should return false if alphabet length is not 26', () => {
+        const alphabet = ["p", "l", "m", "o", "k", "n", "i", "j", "b", "u", "h", "v", "y", "g", "c", "t", "f", "x", "r", "d", "z", "e", "s", "w", "a"];
+        const input = "ykrrpik";
+        const encode = true;
+
+        const expected = false;
+        const actual = substitution(input, alphabet, encode);
+        expect(actual).to.be.false;
+    });
+    it ('should return false if alphabet contains duplicate letters', () => {
+        const alphabet = ["p", "l", "m", "o", "k", "n", "i", "j", "b", "u", "h", "v", "y", "g", "c", "t", "f", "x", "r", "d", "z", "e", "s", "w", "q", "q"];
+        const input = "ykrrpik";
+        const encode = true;
+
+        const expected = false;
+        const actual = substitution(input, alphabet, encode);
+        expect(actual).to.be.false;
+    });
+    it('should decode to "message"', () => {
+        const alphabet = ["p", "l", "m", "o", "k", "n", "i", "j", "b", "u", "h", "v", "y", "g", "c", "t", "f", "x", "r", "d", "z", "e", "s", "w", "a", "q"];
+        const input = "ykrrpik";
+        const encode = true;
+
+        const expected = "message";
+        const actual = substitution(input, alphabet, encode);
+        expect(actual).to.equal(expected);
+    });
+})
