3df37d0397cf166bed5ef62fa4cfa710691e1c5a
Andy Hotzler <ahotzler@gmail.com>
2021-01-04 11:42:24 -0600

    Wrote basic tests for substitution cipher; one not passing

---
+++ src/db.js
@@ -0,0 +1,11 @@
+// Create a database for ciphers
+
+const db = [
+    { a: "z" },
+    { b: "y" },
+    { c: "x" },
+];
+
+const sum = 42;
+
+module.exports =  { db, sum };
--- src/substitution.js
+++ src/substitution.js
@@ -1,3 +1,23 @@
-function substitution(input, alphabet, encode = true) {}
+const { db } = require('./db');
+
+
+
+function substitution(input, alphabet, encode = true) {
+    console.log(alphabet);
+    if (alphabet.length != 26) false;
+    
+    // Checking for duplicate letters in alphabet
+    for (let i = 0; i < alphabet.length; i++) {
+        for (let j = 1; j < alphabet.length; j++) {
+            if (alphabet[i] === alphabet[j]) {
+                return false;
+            };
+        };
+    };
+
+    return "message";
+}
+
+
 
 module.exports = substitution;
